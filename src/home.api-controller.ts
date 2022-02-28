db5c37260e5e613ac13723d5a006ccafec7b856e
FreeIntelligence <free04381@gmail.com>
2022-02-28 08:59:03 -0300

    Se crean controladores para hacer pruebas.

---
+++ src/home.api-controller.ts
@@ -0,0 +1,11 @@
+import { BaseApiController } from '@dyna/http-api-router';
+
+//@ApiController()
+export class HomeApiController extends BaseApiController {
+
+  //@Get('/')
+  public index() {
+
+  }
+
+}
---
+++ src/profile/profile.api-controller.ts
@@ -0,0 +1,11 @@
+import { BaseApiController } from '@dyna/http-api-router';
+
+//@ApiController()
+export class ProfileApiController extends BaseApiController {
+
+  //@Get('/')
+  public index() {
+
+  }
+
+}
