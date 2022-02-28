b2b05d2d6afc5dbdeaa5571f8249d0bb60697209
FreeIntelligence <free04381@gmail.com>
2022-02-28 05:04:14 -0300

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import './libraries.ts';
 //import '@dyna/dev/libraries';
 import { Application } from '@dyna/core';
+import * as path from 'path';
 
-const app = new Application(__dirname);
+const app = new Application(__dirname, path.join(__dirname, '..', 'package.json'), path.join(__dirname, '..', 'libraries'));
 
+app.autoImport();
 app.runInitializers();
