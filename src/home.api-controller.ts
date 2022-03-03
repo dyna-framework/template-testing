9a35c0d7e36f6aa32d5ae4eb452782dda7a06f90
FreeIntelligence <free04381@gmail.com>
2022-03-02 22:08:46 -0300

    Prettier..

--- src/home.api-controller.ts
+++ src/home.api-controller.ts
@@ -1,11 +1,12 @@
-import { BaseApiController } from '@dyna/http-api-router';
-import { ApiController, Get } from '@dyna/http-api-router';
+import { BaseApiController, Post, Put } from '@dyna/http-api-router';
+import { ApiController, Get, Method } from '@dyna/http-api-router';
 
 @ApiController()
 export class HomeApiController extends BaseApiController {
 
-  @Get('/')
-  @Get('/home')
+  @Put('/')
+  @Post('/home')
+  @Method('any')
   public index() {
     console.log('home.api-controller.ts -> index');
     return 'home.api-controller.ts -> index';
