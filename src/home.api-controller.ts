694b9bb91f7461b1d0cb8ca1b97b51178e240af5
FreeIntelligence <free04381@gmail.com>
2022-03-06 08:33:17 -0300

    Pruebas..

--- src/home.api-controller.ts
+++ src/home.api-controller.ts
@@ -1,17 +1,22 @@
-import { Any, BaseHttpController, NativeRequest, NativeResponse, Post, Put, Response } from '@dyna/http-router'
+import { Any, BaseHttpController, Input, NativeRequest, NativeResponse, Post, Put, Response } from '@dyna/http-router'
 import { HttpController, Get, Method } from '@dyna/http-router'
 import { IncomingMessage, ServerResponse } from 'http'
+
+"/"
+"/index"
+
+"/get-users"
 
 @HttpController()
 export class HomeApiController extends BaseHttpController {
   @Get()
-  index(@NativeRequest() req: IncomingMessage) {
+  index(@Input() req: IncomingMessage) {
     console.log('home.api-controller.ts -> index')
     const d = { test: { prueba: 'con una respuesta' } }
     return (d as any).test.prueba
   }
 
-  @Get()
+  @Get('/get-users')
   users() {
     return [{ id: 1, name: 'FreeIntelligence' }]
   }
